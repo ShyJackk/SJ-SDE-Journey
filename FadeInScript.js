15e33bd92f01b46a837767dfec897aa6c915841d
ShyJackk <143224443+ShyJackk@users.noreply.github.com>
2023-11-30 12:14:18 -0600

    Add files via upload
    Add in new scripts

---
+++ FadeInScript.js
@@ -0,0 +1,21 @@
+// Fade and Slide In
+
+const items = document.querySelectorAll('.item:not(:first-child)');
+
+const options = {
+  threshold: 0.5
+}
+
+function addSlideIn(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    }
+  });
+}
+
+const observer = new IntersectionObserver(addSlideIn, options)
+
+items.forEach(item => {
+  observer.observe(item);
+})
